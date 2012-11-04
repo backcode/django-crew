763ad0bc4c4ad2a10394c6612003cde43968dd61
Grant Viklund <gviklund@backcode.com>
2012-11-04 12:11:13 -0800

    Added Team, Role and Membership

--- crewproject/crew/models.py
+++ crewproject/crew/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models import ObjectDoesNotExist
+from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from django_extensions.db.models import TimeStampedModel
+
+
+class Team(TimeStampedModel):
+	name = models.CharField(_("Team Name"), max_length="30", null=True, blank=True)
+	owner = models.OneToOneField(User)
+    members = models.ManyToManyField(User, through='Membership')
+
+
+class Role(TimeStampedModel):
+	name = models.CharField(_("Role"), max_length="30", null=True, blank=True)
+
+
+class Membership(models.Model):
+	user = models.ForeignKey(User)
+    team = models.ForeignKey(Team)
+    
+	role = models.ForeignKey(Role)
