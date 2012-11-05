b6a638132aadd52c6bce0003fe7e83acfee3ff6d
Grant Viklund <gviklund@backcode.com>
2012-11-04 18:46:15 -0800

    Simplified model design

--- crewproject/crew/models.py
+++ crewproject/crew/models.py
@@ -1,36 +1,53 @@
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+#from django.core.urlresolvers import reverse
 from django.db import models
-from django.db.models import ObjectDoesNotExist
+#from django.db.models import ObjectDoesNotExist
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
 
 from django_extensions.db.models import TimeStampedModel
 
 
-class Team(TimeStampedModel):
-	"""We are defining a team of people (group) which we will use to organize users."""
-	name = models.CharField(_("Team Name"), max_length="30", null=True, blank=True)
-	owner = models.OneToOneField(User)
-    members = models.ManyToManyField(User, through='Membership')
+ROLE_CHOICES = (
+    (0, 'Read'),
+    (1, 'Write'),
+    (2, 'Admin'),
+)
 
 
-class Role(TimeStampedModel):
-	"""A Role is user defineable to give flexibility in how you structure your project."""
-	name = models.CharField(_("Role"), max_length="30", null=True, blank=True)
-	read = models.BooleanField(_("Read"))
-	write = models.BooleanField(_("Write"))
-	admin = models.BooleanField(_("Admin"))
+class Crew(TimeStampedModel):
+    """Defining a team of people (group) to be used to organize users."""
+    name = models.CharField(_("Crew Name"), max_length="30", null=True, blank=True)
+    owner = models.OneToOneField(User)
+    members = models.ManyToManyField(User, through='Membership', related_name='+')
+    slug = models.SlugField(_("Slug"), blank=True, null=True, max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super(Crew, self).save(*args, **kwargs)
+        if not self.slug:
+            # TODO - make unique
+            slug = slugify(self.name)
+            #try:
+            #    Step.objects.get(slug=slug, protocol=self.protocol)
+            #    count = self.protocol.step_set.filter(slug=slug).count()
+            #    self.slug = "{0}-{1}".format(slug, count)
+            #except ObjectDoesNotExist:
+            self.slug = slug
+            self.save()
+
 
 
 class Membership(models.Model):
-	"""This is the linking table for definig who is on a team and what their role on the team is.  A use may only have one Role per team."""
-	user = models.ForeignKey(User)
-    team = models.ForeignKey(Team)
-	role = models.ForeignKey(Role)
+    """This is the linking table for definig who is on a team and what their role on the team is.  A use may only have one Role per team."""
+    user = models.ForeignKey(User)
+    team = models.ForeignKey('crew.Crew')
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
 
     class Meta:
         ordering = ['team__name', 'user__username']
 
     def __unicode__(self):
-    	return "%s is a %s on %s" % (self.user, self.role, self.team)
+        return "%s is a %s on %s" % (self.user, self.role, self.team)
--- crewproject/crew/views.py
+++ crewproject/crew/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+from django.core.urlresolvers import reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
+from crew.models import Crew
+
+
+class CrewDetailView(DetailView):
+
+    model = Crew
+
+    def get_context_data(self, **kwargs):
+        context = super(CrewDetailView, self).get_context_data(**kwargs)
+        #context['breadcrumbs'].append((self.object.get_absolute_url(), self.object.name), )
+        #context['actions'] = self.object.action_set.select_related()
+
+        return context
